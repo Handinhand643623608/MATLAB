e4592095039a26e5b040948f63a410d42a2a003b
JoshGrooms <josh.grooms@gmail.com>
2014-11-24 13:47:11 -0500

    Empirical CDF Documentation Update
    Validated the outputs of the mex function against a native MATLAB
    algorithm and a canonical (i.e. theoretical) normal distribution. This
    function is performing as expected. Updated the documentation across all
    documents.

--- Special Functions/Statistics/Mex/MexEmpiricalCDF.c
+++ Special Functions/Statistics/Mex/MexEmpiricalCDF.c
@@ -5,18 +5,26 @@
  *
  *	OUTPUT:
  *		p:		[ N x 1 DOUBLES ]
- *				An array of p-values corresponding with the data in r.
+ *				An array of p-values corresponding with the data in r. This will always be a column vector of
+ *				double-precision numbers that is the same length as r (length L). Each p-value in this vector is
+ *				associated with the corresponding value in r.
  *
  *	INPUTS:
  *		r:		[ N x 1 DOUBLES ] 
- *				The real data distribution.
+ *				The real data distribution. This should be the data that will be tested for statistical significance
+ *				after conversion to p-values. This must be a column vector of double-precision numbers of length L.
+ *				Additionally, any NaNs or zeros in this vector must be removed prior to invoking this function.
  *
  *		n:		[ M x 1 DOUBLES ]
- *				The null data distribution. 
+ *				The null data distribution. This should be an empirically derived null data distribution, which is an
+ *				estimate of what the data in r would look like if the null hypothesis that is to be tested is in fact
+ *				true. Like r, this must be a column vector of double-precision numbers, but the lengths of r and n do
+ *				not necessarily have to agree. Additionally, any NaNs or zeros in this vector must be removed prior to
+ *				invoking this function.
  */
 
 /* CHANGELOG
- * Written by Josh Grooms on 20141121
+ *	Written by Josh Grooms on 20141121
  */
 
 // DEPENDENCIES
--- Special Functions/Statistics/Mex/MexEmpiricalCDF.h
+++ Special Functions/Statistics/Mex/MexEmpiricalCDF.h
@@ -1,5 +1,31 @@
+/* MEXEMPIRICALCDF - Generates p-values for data using an empirically derived null distribution.
+ *
+ *	SYNTAX:
+ *		p = MexEmpiricalCDF(r, n)
+ *
+ *	OUTPUT:
+ *		p:		[ N x 1 DOUBLES ]
+ *				An array of p-values corresponding with the data in r. This will always be a column vector of
+ *				double-precision numbers that is the same length as r (length L). Each p-value in this vector is
+ *				associated with the corresponding value in r.
+ *
+ *	INPUTS:
+ *		r:		[ N x 1 DOUBLES ] 
+ *				The real data distribution. This should be the data that will be tested for statistical significance
+ *				after conversion to p-values. This must be a column vector of double-precision numbers of length L.
+ *				Additionally, any NaNs or zeros in this vector must be removed prior to invoking this function.
+ *
+ *		n:		[ M x 1 DOUBLES ]
+ *				The null data distribution. This should be an empirically derived null data distribution, which is an
+ *				estimate of what the data in r would look like if the null hypothesis that is to be tested is in fact
+ *				true. Like r, this must be a column vector of double-precision numbers, but the lengths of r and n do
+ *				not necessarily have to agree. Additionally, any NaNs or zeros in this vector must be removed prior to
+ *				invoking this function.
+ */
 
-
+/* CHANGELOG
+ *	Written by Josh Grooms on 20141121
+ */
 
 /// <summary>
 ///	Serves as the entry point to the MEX function that generates empirical CDFs.
