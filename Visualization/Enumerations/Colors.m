580ae48672c178c2419c39c7e4be5a38d9412514
JoshGrooms <josh.grooms@gmail.com>
2015-03-30 10:20:55 -0400

    New Classes Color, Colors, & Colormaps
    Implemented a new Color class to exert some control over coloring across
    my visualization code. Also implemented a number of static utility
    constructors to handle data conversions. Some work still needs to be
    done on this class, but it's mostly functional for the time being. Color
    spaces and conversions between them still need to be implemented.
    
    I also implemented two "enumeration" classes associated with Color. The
    Colors class provides quick access to several of the colors that I
    regularly use as Color objects. Similarly, the Colormaps class provides
    quick access to color mappings (mostly from MATLAB-native ones) as
    arrays of Color objects. It also provides a simple utility for quickly
    creating new, custom color mappings.

---
+++ Visualization/Enumerations/Colors.m
@@ -0,0 +1,34 @@
+classdef Colors
+% COLORS - 
+
+%% CHANGELOG
+%   Written by Josh Grooms on 20150211
+
+
+    
+    properties (Constant)
+        
+        % Principal Colors
+        Black       = Color(0, 0, 0);			% The color black	(RGB = [0, 0, 0]).
+        Blue        = Color(0, 0, 1);			% The color blue	(RGB = [0, 0, 1]).
+        Cyan        = Color(0, 1, 1);			% The color cyan	(RGB = [0, 1, 1]).
+        Gray        = Color(0.5, 0.5, 0.5);		% The color gray	(RGB = [0.5, 0.5, 0.5]).
+        Green       = Color(0, 1, 0);			% The color green	(RGB = [0, 1, 0]).
+        Magenta     = Color(1, 0, 1);			% The color magenta (RGB = [1, 0, 1]).
+		Orange		= Color(1, 0.5, 0);
+        Red         = Color(1, 0, 0);			% The color red		(RGB = [1, 0, 0]).
+        White       = Color(1, 1, 1);			% The color white	(RGB = [1, 1, 1]).
+		Violet		= Color(0.5, 0, 1);
+        Yellow      = Color(1, 1, 0);			% The color yellow	(RGB = [1, 1, 0]).
+    
+        % Others
+        VibrantBlue = Color(0, 0.75, 1);
+        
+    end
+        
+    
+    
+    
+    
+    
+end
---
+++ Visualization/Enumerations/Colorspace.m
@@ -0,0 +1,17 @@
+classdef Colorspace
+% COLORSPACE - 
+
+%% CHANGELOG
+%	Written by Josh Grooms on 20150218
+	
+
+
+	enumeration
+		HSL
+		HSV
+		RGB
+	end
+	
+	
+	
+end
