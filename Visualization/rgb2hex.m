e5a06e0a64e17776ce6c27140dc44fccabcb0f7d
JoshGrooms <josh.grooms@gmail.com>
2015-07-23 15:16:50 -0700

    Bug Fix for "rgb2hex" Function
    Fixed a bug where zeros weren't being properly prepended to hex codes
    with single characters.

--- Visualization/rgb2hex.m
+++ Visualization/rgb2hex.m
@@ -27,6 +27,7 @@ function h = rgb2hex(r, g, b)
 
 %% CHANGELOG
 %   Written by Josh Grooms on 20141023
+%		20150603:	Bug fix for the code that prepends zeros to any single-character hex codes that are found.
 
 
 
@@ -36,9 +37,11 @@ assert(all([r, g, b] <= [255, 255, 255]), 'RGB color values must always be less 
 assert(all([r, g, b] >= [0, 0, 0]), 'RGB color values must always be greater than 0.');
 
 % Convert to individual hex entries
-h = {dec2hex(r) dec2hex(g) dec2hex(b)};
+h = { dec2hex(r) dec2hex(g) dec2hex(b) };
 
 % Ensure that each entry has two characters, then concatenate & return
 hexIsSingleChar = cellfun(@(x) length(x) == 1, h);
-if (any(hexIsSingleChar)); h{hexIsSingleChar} = ['0' h{hexIsSingleChar}]; end
+if any(hexIsSingleChar)
+	h(hexIsSingleChar) = cellfun(@(x) ['0' x], h(hexIsSingleChar), 'UniformOutput', false);
+end
 h = [h{:}];
