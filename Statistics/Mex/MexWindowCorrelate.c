b6a5f490669e89a94e536bcf641f7e27af388aa4
gth775q <josh.grooms@gmail.com>
2015-08-24 11:18:32 -0700

    Updated sliding window correlation C code for compatibility with Unix platforms. Also recompiled this routine as a .mexa64 binary. Tested to run correctly on my personal Ubuntu installation, but invalid CPU flags during compilation have caused issues in the past on machines other than my own, so I'll have to check on that at some point in the future.

--- Statistics/Mex/MexWindowCorrelate.c
+++ Statistics/Mex/MexWindowCorrelate.c
@@ -1,8 +1,8 @@
 /* MEXWINDOWCORRELATE - Computes the sliding window correlation between an array of signals and one or more other signals.
- * 
- *	MEXWINDOWCORRELATE computes the sliding window correlation between two sets of signals over time. This creates a set of 
- *	time series showing how correlation between two data sets changes as a function of time. It is particularly useful in  
- *	analysing relationships between data that have non-stationary or time-varying statistical properties (such as my fMRI and 
+ *
+ *	MEXWINDOWCORRELATE computes the sliding window correlation between two sets of signals over time. This creates a set of
+ *	time series showing how correlation between two data sets changes as a function of time. It is particularly useful in
+ *	analysing relationships between data that have non-stationary or time-varying statistical properties (such as my fMRI and
  *	EEG data).
  *
  *	SYNTAX:
@@ -10,36 +10,36 @@
  *
  *	OUTPUT:
  *		swc:			[ MC x NC DOUBLES ]
- *						An array of sliding window correlation values calculated between the data in X and Y. Each row of 
- *						this array contains Pearson correlation coefficients (i.e. r values) between a specific segment of 
+ *						An array of sliding window correlation values calculated between the data in X and Y. Each row of
+ *						this array contains Pearson correlation coefficients (i.e. r values) between a specific segment of
  *						the signals in X and Y. The number of correlation time points MC will always follow this formula:
  *
  *							MC = floor( (M - WINDOW) / (WINDOW - NOVERLAP) )
- *						
- *						The number of correlation signals NC in this array will always follow NC = NX * NY in order to hold 
- *						all possible pairings of signals. Each successive column in SWC then represents the correlation over 
- *						time between one signal in Y and a successive signal in X. Each grouping of NX columns in this array 
- *						therefore corresponds with the correlation between one signal in Y and all signals in X. Successive 
+ *
+ *						The number of correlation signals NC in this array will always follow NC = NX * NY in order to hold
+ *						all possible pairings of signals. Each successive column in SWC then represents the correlation over
+ *						time between one signal in Y and a successive signal in X. Each grouping of NX columns in this array
+ *						therefore corresponds with the correlation between one signal in Y and all signals in X. Successive
  *						groupings correspond with successive signals in Y.
  *
  *	INPUTS:
  *		x:				[ M x NX DOUBLES ]
- *						An array of doubles containing the signal(s) to be correlated with each signal in Y. Each column of 
- *						this array represents a single signal with M time points. The number of signals NX is free to vary 
+ *						An array of doubles containing the signal(s) to be correlated with each signal in Y. Each column of
+ *						this array represents a single signal with M time points. The number of signals NX is free to vary
  *						but must be a positive integer. The number of time points M must always equal M from Y.
  *
  *		y:				[ M x NY DOUBLES ]
- *						An array of doubles containing the signal(s) to be correlated with each signal in X. Each column of 
- *						this array represents a single signal with M time points. The number of signals NY is free to vary 
+ *						An array of doubles containing the signal(s) to be correlated with each signal in X. Each column of
+ *						this array represents a single signal with M time points. The number of signals NY is free to vary
  *						but must be a positive integer. The number of time points M must always equal M from X.
  *
  *		window:			INT
- *						The number of samples that constitute a single window. More specifically, this argument is the length 
- *						of the window in signal samples. 
+ *						The number of samples that constitute a single window. More specifically, this argument is the length
+ *						of the window in signal samples.
  *
  *		noverlap:		INT
- *						The number of samples to be reused in successive correlation estimates. This is how many sample 
- *						points are "overlapped" from previous estimates as the window slides along a signal. This argument 
+ *						The number of samples to be reused in successive correlation estimates. This is how many sample
+ *						points are "overlapped" from previous estimates as the window slides along a signal. This argument
  *						must be an integer between 0 and WINDOW - 1.
  *
  *	See also: CCORR, SWCORR
@@ -49,7 +49,7 @@
  * Written by Josh Grooms on 20150203
  */
 
-#include <cilk\cilk.h>
+#include <cilk/cilk.h>
 #include <mathimf.h>
 #include <mex.h>
 
@@ -82,7 +82,7 @@ void mexFunction(int nargout, mxArray* argout[], int nargin, const mxArray* argi
 	if (nrx == 0 || nry == 0) { mexErrMsgTxt("Inputs cannot be empty arrays."); }
 	if (nrx != nry) { mexErrMsgTxt("X and Y must contain equivalent length signals."); }
 
-	// We need a higher precision calculation for the number of SWC points per signal. Otherwise, if this ends up being fractional, 
+	// We need a higher precision calculation for the number of SWC points per signal. Otherwise, if this ends up being fractional,
 	// it could get rounded up and result in out-of-bounds indexing later on. We need to always force it downward.
 	float temp = (float)(nrx - window) / (float)increment;
 	int nswc = (int)floor(temp);
