91bf3778cef869d20107d357f12942626c42332b
JoshGrooms <josh.grooms@gmail.com>
2015-08-28 12:03:07 -0700

    Updated function documentation.

--- File Management/where.m
+++ File Management/where.m
@@ -9,21 +9,21 @@
 %   contains the file instead of pointing to the file itself.
 %
 %   SYNTAX:
-%		p = where(fileName)
+%       p = where(fileName)
 %
 %   OUTPUT:
-%		p:				STRING
-%						A path string pointing to the folder where the inputted file exists on the hard drive. If the file name
-%						being searched for does not exist or is not in a directory that is included in MATLAB's working path
-%						list, then this will be an empty string. Otherwise, it will be the full path to the folder without
-%						the file name or extension included at the end.
+%       p:              STRING
+%                       A path string pointing to the folder where the inputted file exists on the hard drive. If the file
+%                       name being searched for does not exist or is not in a directory that is included in MATLAB's working
+%                       path list, then this will be an empty string. Otherwise, it will be the full path to the folder
+%                       without the file name or extension included at the end.
 %
 %   INPUT:
-%		fileName:       STRING
-%						The name of the file or function to be located. Including the file's extension is optional. Any given
-%						name that is inputted here must refer to a file or function that is on MATLAB's working path in order
-%						for it to be successfully found. Otherwise, the search will return an empty string even if the
-%						desired file does indeed exist.
+%        fileName:      STRING
+%                       The name of the file or function to be located. Including the file's extension is optional. Any given
+%                       name that is inputted here must refer to a file or function that is on MATLAB's working path in order
+%                       for it to be successfully found. Otherwise, the search will return an empty string even if the
+%                       desired file does indeed exist.
 %
 %   See also: CONTENTS, DIR, FILE, PATH, SEARCHDIR, WHICH
 
